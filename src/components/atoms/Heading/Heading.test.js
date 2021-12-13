6b6eee9fb90cff4df5827f8fc9e27e0edeb9cc09
KamilPartyka <kamilkrzysztofpartyka@gmail.com>
2021-12-13 21:25:09 +0100

    first test

---
+++ src/components/atoms/Heading/Heading.test.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
+import Heading from './Heading';
+
+describe('Heading Component', () => {
+  test('Renders children text', () => {
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <Heading>Hello</Heading>
+      </ThemeProvider>,
+    );
+
+    getByText('Hello');
+  });
+});
